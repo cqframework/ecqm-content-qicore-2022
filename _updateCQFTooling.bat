1c8e0a6b63c796b22b51823e6365ce7ac74bbdc6
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2022-04-12 11:37:09 -0600

    Updated to latest templates and scripts

--- _updateCQFTooling.bat
+++ _updateCQFTooling.bat
@@ -1,6 +1,4 @@
 @ECHO OFF
-REM Necessary for the prompting of 'create' and 'overwrite' variables as, without this, they will be expanded immediately to '' and promptig will have no effect.
-setlocal EnableDelayedExpansion
 
 SET "dlurl=https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.opencds.cqf&a=tooling&v=1.4.1-SNAPSHOT&c=jar-with-dependencies"
 SET tooling_jar=tooling-1.4.1-SNAPSHOT-jar-with-dependencies.jar
@@ -11,56 +9,44 @@ IF "%~1"=="/f" SET skipPrompts=true
 FOR %%x IN ("%CD%") DO SET upper_path=%%~dpx
 
 IF NOT EXIST "%input_cache_path%%tooling_jar%" (
-	IF NOT EXIST "%upper_path%%tooling_jar%" (
-		SET jarlocation=%input_cache_path%%tooling_jar%
-		SET jarlocationname=Input Cache
-		ECHO IG Refresh is not yet in input-cache or parent folder.
-		REM we don't use jarlocation below because it will be empty because we're in a bracketed if statement
-		GOTO create
-	) ELSE (
-		ECHO IG RefreshFOUND in parent folder
-		SET jarlocation=%upper_path%%tooling_jar%
-		SET jarlocationname=Parent folder
-		GOTO:upgrade
-	)
+   IF NOT EXIST "%upper_path%%tooling_jar%" (
+      SET jarlocation=%input_cache_path%%tooling_jar%
+      SET jarlocationname=Input Cache
+      ECHO IG Refresh is not yet in input-cache or parent folder.
+      REM we don't use jarlocation below because it will be empty because we're in a bracketed if statement
+      GOTO create
+   ) ELSE (
+      ECHO IG RefreshFOUND in parent folder
+      SET jarlocation=%upper_path%%tooling_jar%
+      SET jarlocationname=Parent folder
+      GOTO:upgrade
+   )
 ) ELSE (
-	ECHO IG Refresh FOUND in input-cache
-	SET jarlocation=%input_cache_path%%tooling_jar%
-	SET jarlocationname=Input Cache
-	GOTO:upgrade
+   ECHO IG Refresh FOUND in input-cache
+   SET jarlocation=%input_cache_path%%tooling_jar%
+   SET jarlocationname=Input Cache
+   GOTO:upgrade
 )
 
 :create
 ECHO Will place refresh jar here: %input_cache_path%%tooling_jar%
 IF "%skipPrompts%"=="false" (
-    SET /p create="Ok? (Y/N)"
-    IF /I "!create!"=="Y" (
-        ECHO Creating refresh jar
-        MKDIR "%input_cache_path%" 2> NUL
-        GOTO:download
-    ) ELSE (
-        ECHO Skipping create
-        GOTO:done
-    )
-) ELSE (
-    ECHO Prompting disabled, creating refresh jar
-    MKDIR "%input_cache_path%" 2> NUL
-    GOTO:download
-)
+    SET /p create=Ok? [Y/N]
+    IF /I "%create%"=="Y" goto:mkdir
+) ELSE goto:mkdir
+
 GOTO:done
+:mkdir
+    mkdir "%input_cache_path%" 2> NUL
+GOTO:download
 
 :upgrade
 IF "%skipPrompts%"=="false" (
     SET /p overwrite="Overwrite %jarlocation%? (Y/N)"
-    IF /I "!overwrite!"=="Y" (
-        ECHO Overwriting existing jar
+    IF /I "%overwrite%"=="Y" (
         GOTO:download
-    ) ELSE (
-        ECHO Skipping download as it would overwrite existing jar
-        GOTO:done
     )
 ) ELSE (
-    ECHO Prompting disabled, overwriting refresh jar
     GOTO:download
 )
 GOTO:done
@@ -91,7 +77,7 @@ GOTO done
 :win8.1
 :win8
 :vista
-ECHO "This script does not yet support Windows %winver%.  Please ask for help on https://chat.fhir.org/#narrow/stream/179207-connectathon-mgmt/topic/Clinical.20Reasoning.20Track"
+ECHO This script does not yet support Windows %winver%.  Please ask for help on https://chat.fhir.org/#narrow/stream/179207-connectathon-mgmt/topic/Clinical.20Reasoning.20Track
 GOTO done
 
 :done
